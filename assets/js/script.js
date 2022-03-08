79f4e6657a0e2027f13253943bd72b1cafb9a459
Samuel Gonzalez <samglz10@gmail.com>
2022-03-07 20:57:26 -0500

    first update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 // TODO: Declare any global variables we need
 
+let headsRoll = 0
+let tailsRoll = 0
+
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+  
 
     // TODO: Add event listener and handler for flip and clear buttons
+    document.addEventListener('flip').addEventListener ('click', () 
 
     // Flip Button Click Handler
         // TODO: Determine flip outcome
